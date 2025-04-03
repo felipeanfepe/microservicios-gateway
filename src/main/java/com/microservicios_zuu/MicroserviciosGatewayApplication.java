a95ef871d559995987ef75a8f7c9366d6ea9bbee
Andres Felipe Perdomo Andrade <felipeanfepe@gmail.com>
2025-04-02 20:15:25 -0500

    feat: gatewaty

--- src/main/java/com/microservicios_zuu/MicroserviciosGatewayApplication.java
+++ src/main/java/com/microservicios_zuu/MicroserviciosGatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroserviciosZuulApplication {
+public class MicroserviciosGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviciosZuulApplication.class, args);
+		SpringApplication.run(MicroserviciosGatewayApplication.class, args);
 	}
 
 }
